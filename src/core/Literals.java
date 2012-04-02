7bceeb57097a2a94bec91a433ad3c8cd64a2e13e
Jiri Sitina <jiri.sitina@gooddata.com>
2012-04-02 15:32:42 +0200

    Initial commit

---
+++ src/core/Literals.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2007-2012, GoodData(R) Corporation. All rights reserved.
+ */
+package core;
+
+public class Literals {
+
+    int one_million = 1_000_000;
+
+}
---
+++ src/core/ResourceManagement.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2007-2012, GoodData(R) Corporation. All rights reserved.
+ */
+package core;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ResourceManagement {
+
+    public static void main(String[] args) throws Exception {
+        String path = "src/com/gooddata/java7/examples/core/ResourceManagement.java";
+        System.out.println(readLineJava7(path));
+    }
+
+    public static String readLineJava7(String path) throws Exception {
+        try (
+                FileReader fr = new FileReader(path);
+                BufferedReader br = new BufferedReader(fr)) {
+            return br.readLine();
+        }
+    }
+
+}
