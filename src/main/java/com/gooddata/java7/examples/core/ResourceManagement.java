6450010347c2f85771b33be5f3dc469232574255
Jiri Sitina <jiri.sitina@gooddata.com>
2012-04-12 20:29:13 +0200

    Update of ResourceManagement example to demonstrate order of resources closing.

--- src/main/java/com/gooddata/java7/examples/core/ResourceManagement.java
+++ src/main/java/com/gooddata/java7/examples/core/ResourceManagement.java
@@ -4,20 +4,51 @@
 package com.gooddata.java7.examples.core;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 
 public class ResourceManagement {
 
     public static void main(String[] args) throws Exception {
-        String path = "src/com/gooddata/java7/examples/core/ResourceManagement.java";
+        String path = "src/main/java/com/gooddata/java7/examples/core/ResourceManagement.java";
         System.out.println(readLine(path));
     }
 
     public static String readLine(String path) throws Exception {
         // resources must implement AutoCloseable interface
-        try (FileReader fr = new FileReader(path); BufferedReader br = new BufferedReader(fr)) {
+        try (FileReader fr = new ExtFileReader(path); BufferedReader br = new ExtBufferedReader(fr)) {
             return br.readLine();
         }
+    }
+
+    public static class ExtFileReader extends FileReader {
+
+        public ExtFileReader(String fileName) throws FileNotFoundException {
+            super(fileName);
+        }
+
+        @Override
+        public void close() throws IOException {
+            super.close();
+            System.out.println("FileReader close");
+        }
+
+    }
+
+    public static class ExtBufferedReader extends BufferedReader {
+
+        public ExtBufferedReader(Reader in) {
+            super(in);
+        }
+
+        @Override
+        public void close() throws IOException {
+            super.close();
+            System.out.println("BufferedReader close");
+        }
+
     }
 
 }
